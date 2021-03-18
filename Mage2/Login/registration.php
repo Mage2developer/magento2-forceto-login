ca67490939d962d35a836227fc7329faa2d6eaa8
Mage2Developer <mage2developer@gmail.com>
2021-03-18 11:56:43 +0530

    Update copyright content

--- Mage2/Login/registration.php
+++ Mage2/Login/registration.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * Product Name: Mage2 Force to Login
- * Module Name: Mage2_Login
- * Created By: Yogesh Shishangiya
+ * Mage2developer
+ * Copyright (C) 2021 Mage2developer
+ *
+ * @category Mage2developer
+ * @package Mage2_Login
+ * @copyright Copyright (c) 2021 Mage2developer
+ * @author Mage2developer <mage2developer@gmail.com>
  */
 
 use Magento\Framework\Component\ComponentRegistrar;
